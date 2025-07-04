b57201e1c5e2298740879fc3a50bf587ffee4ea6
Clay1442 <clayjose20@gmail.com>
2025-07-04 16:53:12 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/claydev/course/resources/OrderResource.java
+++ src/main/java/com/claydev/course/resources/OrderResource.java
@@ -2,6 +2,7 @@ package com.claydev.course.resources;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,28 +10,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.claydev.course.entities.Order;
+import com.claydev.course.services.OrderService;
 
-import com.claydev.course.entities.User;
-import com.claydev.course.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/orders")
+public class OrderResource {
 
 	@Autowired
-	private UserService service;
-
+	private OrderService service;
+	
+	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		List<User> list = service.findAll();
-
+     public ResponseEntity<List<Order>> findAll() {
+    	List<Order> list = service.findAll(); 
+		
 		return ResponseEntity.ok().body(list);
-	}
-
+     }
+	 
+	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User user = service.findById(id);
-
-		return ResponseEntity.ok().body(user);
-	}
+	 public ResponseEntity<Order> findById(@PathVariable Long id){
+		Order order = service.findById(id);
+		 
+		 return ResponseEntity.ok().body(order);
+	 } 
+	
 }
