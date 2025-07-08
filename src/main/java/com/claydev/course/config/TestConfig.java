dad1df210d8c9f9e17d1d6d21ffcf6c7d4adf6de
Clay1442 <clayjose20@gmail.com>
2025-07-08 12:00:48 -0300

    Payment, one-to-one association

--- src/main/java/com/claydev/course/config/TestConfig.java
+++ src/main/java/com/claydev/course/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.claydev.course.entities.Category;
 import com.claydev.course.entities.Order;
 import com.claydev.course.entities.OrderItem;
+import com.claydev.course.entities.Payment;
 import com.claydev.course.entities.Product;
 import com.claydev.course.entities.User;
 import com.claydev.course.entities.enums.OrderStatus;
@@ -84,6 +85,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	}
